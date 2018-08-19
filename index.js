d88efb3fd86ec925d9989fff82a15513003244be
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-19 22:43:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ class Driver {
   }
 
   passengers() {
-    // debugger
     let passenger_ids = this.trips().map(trip => {return trip.passengerId})
     let passengers = passenger_ids.map(id => {
       return store.passengers.find(passenger => {
