680812ba54b5d959479e51d8e4a03b86b492021b
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-19 21:57:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class Driver {
 
     store.drivers.push(this)
   }
-  
+
   trips() {
     return store.trips.filter(function(trip) {
       return trip.driverId === this.id
@@ -60,9 +60,9 @@ class Trip {
   }
 
   passenger() {
-    return store.passengers.find(passenger => {
-      return passenger.id === this.passengerId
-    })
+    // return store.passengers.find(passenger => {
+    //   return passenger.id === this.passengerId
+    // })
   }
 
   driver() {
