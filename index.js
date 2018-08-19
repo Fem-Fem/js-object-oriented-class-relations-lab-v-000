3c5ad57995cad179ef1f644a8e80406292ae94c1
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-19 20:52:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ class Passenger {
 
   drivers() {
     return store.drivers.filter(driver => {
-      return driver.passengerId === this.driverId
+      return driver.passengerId === this.id
     })
   }
 
