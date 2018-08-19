df78acfbdf074f5ab163f030c9a96e3aca7291c3
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-19 20:45:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,8 +37,6 @@ class Passenger {
 
 class Trip {
   constructor(driver, passenger) {
-    this.driver = driver
-    // this.passenger = passenger
     this.id = ++tripId
 
     this.driverId = driver.id
