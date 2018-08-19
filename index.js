611d2645265e62798d4f96b2a79dc166272a178d
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-19 18:58:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ class Trip {
 
   passenger() {
     return store.passengers.find(function(passenger) {
-      return passenger.tripId === this.passengerId
+      return passenger.id === this.passengerId
     })
   }
 
