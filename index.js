03ac114d4bbd0c311ae4ef95e05f29506d37e5b7
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-19 22:05:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class Driver {
 
     store.drivers.push(this)
   }
-  
+
   trips() {
     return store.trips.filter(trip => {
       return trip.driverId === this.id
@@ -77,23 +77,3 @@ class Trip {
 
 
 }
-// Driver class:
-//
-//     A driver has many trips, and has many passengers through trips.
-//     new Driver() - initialized with a name; returns a JavaScript object that has attributes of id, and name
-//     trips() - returns all of the trips that a driver has taken
-//     passengers() - returns all of the passengers that a driver has taken on a trip
-//
-// Passenger class:
-//
-//     A passenger has many trips, and has many drivers through trips.
-//     new Passenger() - initialized with a name; returns a JavaScript object that has attributes of id, and name
-//     trips() - returns all of the trips that a passenger has taken
-//     drivers() - returns all of the drivers that has taken a passenger on a trip
-//
-// Trip class:
-//
-//     A trip belongs to a driver and belongs to a passenger.
-//     new Trip() - initialized with an instance of driver and an instance of passenger; returns a JavaScript that has attributes id, driverId, and passengerId
-//     driver() - returns the driver associated with the trip
-//     passenger() - returns the passenger associated with the trip
